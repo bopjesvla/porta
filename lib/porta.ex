606d7efc3aa7d9c3dbe8f909f8713740f088f51d
Bob <negenentwintig@hotmail.com>
2017-02-10 14:52:38 +0100

    init

---
+++ lib/porta.ex
@@ -0,0 +1,18 @@
+defmodule Porta do
+  @moduledoc """
+  Documentation for Porta.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Porta.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/porta_test.exs
@@ -0,0 +1,8 @@
+defmodule PortaTest do
+  use ExUnit.Case
+  doctest Porta
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
